08f83694f5d31fa70a82dbd3962a3d846c981d02
tuandui <tuandui@tsign.cn>
2024-08-05 18:52:40 +0800

    feat:添加husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:vue/vue3-essential",
-        'plugin:prettier/recommended'
+        'plugin:prettier/recommended',
     ],
 
     "parserOptions": {
